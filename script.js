47ee5a9be86b5fe3520964784f14e15bab4a3a2c
M-Rasul <rasulmamashev2@gmail.com>
2023-02-08 15:09:17 +0500

    init: create project with simple modal windows

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+const closeModalFunc = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const showModalFunc = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', showModalFunc);
+}
+closeModal.addEventListener('click', closeModalFunc);
+overlay.addEventListener('click', closeModalFunc);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModalFunc();
+  }
+});
